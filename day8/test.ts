447ea3700528372d8a94d04a0085313fbe754af2
Dmitry Kulakov <atomeistee@gmail.com>
2022-12-08 12:02:40 +0300

    refactor day 8

--- day8/test.ts
+++ day8/test.ts
@@ -1,12 +1,12 @@
 import fs from "fs";
 const input = fs.readFileSync(__dirname + "/input.txt", "utf8");
 let example = `30373
-               25512
-               65332
-               33549
-               35390`;
+25512
+65332
+33549
+35390`;
 
-let lines = input.split("\n").map((line) => line.trim());
+let lines = example.split("\n");
 let visibleTrees = 0;
 
 let matrix: number[][] = [];
@@ -28,86 +28,70 @@ for (let i = 0; i < lines.length; i++) {
 for (let row = 0; row < matrix.length; row++) {
   for (let col = 0; col < matrix[row].length; col++) {
     let tree = matrix[row][col];
-    let left = {
-      visible: true,
-      viewTrees: 0,
-    };
-    let right = {
-      visible: true,
-      viewTrees: 0,
-    };
-    let top = {
-      visible: true,
-      viewTrees: 0,
-    };
-    let bottom = {
-      visible: true,
-      viewTrees: 0,
-    };
+    let directions = [
+      {
+        name: "left",
+        visible: true,
+        viewTrees: 0,
+      },
+      {
+        name: "right",
+        visible: true,
+        viewTrees: 0,
+      },
+      {
+        name: "bottom",
+        visible: true,
+        viewTrees: 0,
+      },
+      {
+        name: "top",
+        visible: true,
+        viewTrees: 0,
+      },
+    ];
 
-    // Check if tree visible in the left direction
-    for (let distance = 1; distance < matrix.length; distance++) {
-      if (matrix[row][col - distance] === undefined) break;
-      let isVisible = matrix[row][col - distance] < tree;
-      if (!isVisible) {
-        left.visible = false;
-        left.viewTrees++;
-        break;
+    for (let direction of directions) {
+      // Check if tree visible in the direction
+      for (let distance = 1; distance < matrix.length; distance++) {
+        let treeInThatDirection: number | undefined | number[];
+        switch (direction.name) {
+          case "left":
+            treeInThatDirection = matrix[row][col - distance];
+            break;
+          case "right":
+            treeInThatDirection = matrix[row][col + distance];
+            break;
+          case "bottom":
+            treeInThatDirection =
+              matrix[row + distance] && matrix[row + distance][col];
+            break;
+          case "top":
+            treeInThatDirection =
+              matrix[row - distance] && matrix[row - distance][col];
+            break;
+          default:
+            break;
+        }
+        if (treeInThatDirection === undefined) break;
+        let isVisible = treeInThatDirection < tree;
+        if (!isVisible) {
+          direction.visible = false;
+          direction.viewTrees++;
+          break;
+        }
+        direction.viewTrees++;
       }
-      left.viewTrees++;
     }
-
-    // Check if tree visible in the right direction
-    for (let distance = 1; distance < matrix.length; distance++) {
-      if (matrix[row][col + distance] === undefined) break;
-      let isVisible = matrix[row][col + distance] < tree;
-      if (!isVisible) {
-        right.visible = false;
-        right.viewTrees++;
-        break;
-      }
-      right.viewTrees++;
-    }
-
-    // Check if tree visible in the bottom direction
-    for (let distance = 1; distance < matrix.length; distance++) {
-      if (matrix[row + distance] === undefined) break;
-      let isVisible = matrix[row + distance][col] < tree;
-      if (!isVisible) {
-        bottom.visible = false;
-        bottom.viewTrees++;
-        break;
-      }
-      bottom.viewTrees++;
-    }
-
-    // Check if tree visible in the top direction
-    for (let distance = 1; distance < matrix.length; distance++) {
-      if (matrix[row - distance] === undefined) break;
-      let isVisible = matrix[row - distance][col] < tree;
-      if (!isVisible) {
-        top.visible = false;
-        top.viewTrees++;
-        break;
-      }
-      top.viewTrees++;
-    }
-    //     console.log(
-    //       `Tree ${tree} at [${row},${col}] is
-    // Left: ${JSON.stringify(left, null, 4)}
-    // Right: ${JSON.stringify(right, null, 4)}
-    // Bottom: ${JSON.stringify(bottom, null, 4)}
-    // Top: ${JSON.stringify(top, null, 4)}`
-    //     );
-    let scenicScore =
-      left.viewTrees * right.viewTrees * bottom.viewTrees * top.viewTrees;
+    let scenicScore = directions.reduce(
+      (score, direction) => score * direction.viewTrees,
+      1
+    );
     if (scenicScore > bestScenicScore) bestScenicScore = scenicScore;
-    if (!(left.visible || right.visible || bottom.visible || top.visible)) {
-      continue;
+    if (directions.some((direction) => direction.visible)) {
+      visibleTrees++;
     }
-    visibleTrees++;
   }
 }
 console.log({ visibleTrees });
 console.log({ bestScenicScore });
-// console.log(matrix);
